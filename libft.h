6675c88d8ab189d04ec0583f61d71011fc150f6b
k-iatrou <kianoush.iatrou5@gmail.com>
2025-06-12 13:58:06 +0200

    first functions

---
+++ libft.h
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kiatrou <kiatrou@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/06/10 16:22:33 by kiatrou           #+#    #+#             */
+/*   Updated: 2025/06/11 14:14:54 by kiatrou          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFT_H
+#define LIBFT_H
+
+#include <string.h>
+#include <stdio.h>
+
+int	ft_isalpha(int c);
+int	ft_isdigit(int c);
+int	ft_isalnum(int c);
+int	ft_isascii(int c);
+int	ft_isprint(int c);
+size_t	ft_strlen(const char *s);
+void	ft_memset(void *s, int c, size_t n);
+void	bzero(void *s, size_t n);
+
+#endif
