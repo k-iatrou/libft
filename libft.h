b0f7ab5ac1f6278f5d0323f66aed3b8f918af11e
k-iatrou <158864273+k-iatrou@users.noreply.github.com>
2025-06-27 01:45:47 +0200

    wrote all fts into the libft

--- libft.h
+++ libft.h
@@ -30,5 +30,14 @@ size_t	ft_strlcpy(char *dest, const char *src, size_t size);
 size_t	ft_strlcat(char *dst, const char *src, size_t size);
 int	ft_toupper(int c);
 int	ft_tolower(int c);
+char	*ft_strchr(const char *s, int c);
+char	*ft_strrchar(const char *s, int c);
+int	ft_strncmp(const char *s1, const char *s2, size_t n);
+void	*ft_memchr(const void *s, int c, size_t n);
+int	ft_memcmp(const void *s1, const void *s2, size_t n);
+char	*ft_strnstr(const char *haystack, const char *needle, size_t len);
+int	ft_atoi(const char *str);
+void	*ft_calloc(size_t count, size_t size);
+char	*ft_strdup(const char *s1);
 
-#endif          
+#endif
