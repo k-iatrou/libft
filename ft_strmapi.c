a6ee30cf1c5c884ef1cc67f3a49c1f874686edf3
k-iatrou <158864273+k-iatrou@users.noreply.github.com>
2025-06-27 02:44:33 +0200

    finished all additional fts

---
+++ ft_strmapi.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strmapi.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/06/27 02:31:01 by marvin            #+#    #+#             */
+/*   Updated: 2025/06/27 02:31:01 by marvin           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char    *ft_strmapi(const char *s, char (*f)(unsigned int, char))
+{
+    unsigned int i;
+    char *res;
+
+    res = malloc((ft_strlen(s) + 1) * sizeof(char));
+    if (!res)
+        return (NULL);
+    i = 0;
+    while (i < ft_strlen(s))
+    {
+        res[i] = (*f)(i, s[i]);
+        i++;
+    }
+    res[i] = 0;
+    return (res);
+}
--- libft.h
+++ libft.h
@@ -39,5 +39,16 @@ char	*ft_strnstr(const char *haystack, const char *needle, size_t len);
 int	ft_atoi(const char *str);
 void	*ft_calloc(size_t count, size_t size);
 char	*ft_strdup(const char *s1);
+char    *ft_substr(const char *s, unsigned int start, size_t len);
+char    *ft_strjoin(const char *s1, const char *s2);
+char    *ft_strtrim(const char *s1, const char *set);
+char    **ft_split(const char *s, char c);
+char    *ft_itoa(int n);
+char    *ft_strmapi(const char *s, char (*f)(unsigned int, char));
+void    ft_striteri(char *s, void (*f)(unsigned int, char*));
+void    ft_putchar_fd(char c, int fd);
+void    ft_putstr_fd(char *str, int fd);
+void    ft_putendl_fd(char *str, int fd);
+void    ft_putnbr_fd(int n, int fd);
 
 #endif
